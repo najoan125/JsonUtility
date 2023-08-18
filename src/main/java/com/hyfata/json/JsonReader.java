9f131153f00e08ecd2ed37102816e3d15f1b02a3
Najoan <68422289+najoan125@users.noreply.github.com>
2023-08-18 21:35:35 +0900

    Added readFromInputStream

--- src/main/java/com/hyfata/json/JsonReader.java
+++ src/main/java/com/hyfata/json/JsonReader.java
@@ -3,19 +3,36 @@ package com.hyfata.json;
 import com.hyfata.json.exceptions.JsonEmptyException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class JsonReader {
     public static JSONObject readFromURL(String url) throws IOException, JsonEmptyException {
         URLConnection connection = Utils.getJsonUrlConnection(url);
-        Scanner scanner = new Scanner(connection.getInputStream(), "UTF-8").useDelimiter("\\A");
+        return readFromInputStream(connection.getInputStream());
+    }
+
+    public static JSONObject readFromURL(String url, int fromLine) throws IOException, JsonEmptyException {
+        URLConnection connection = Utils.getJsonUrlConnection(url);
+        return readFromInputStream(connection.getInputStream(), fromLine);
+    }
+
+    public static JSONObject readFromFile(String path) throws IOException, JsonEmptyException {
+        try (FileInputStream inputStream = new FileInputStream(path)) {
+            return readFromInputStream(inputStream);
+        }
+    }
+
+    public static JSONObject readFromFile(String path, int fromLine) throws IOException, JsonEmptyException {
+        try (FileInputStream inputStream = new FileInputStream(path)) {
+            return readFromInputStream(inputStream, fromLine);
+        }
+    }
+
+    public static JSONObject readFromInputStream(InputStream inputStream) throws JsonEmptyException {
+        Scanner scanner = new Scanner(inputStream, "UTF-8").useDelimiter("\\A");
 
         String json = scanner.hasNext() ? scanner.next() : "";
         if (json.equals("")) {
@@ -24,18 +41,13 @@ public class JsonReader {
         return new JSONObject(json);
     }
 
-    public static JSONObject readFromURL(String url, int fromLine) throws IOException, JsonEmptyException {
-        URLConnection connection = Utils.getJsonUrlConnection(url);
+    public static JSONObject readFromInputStream(InputStream inputStream, int fromLine) throws IOException, JsonEmptyException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
 
-        // 데이터를 UTF-8로 읽을 수 있는 reader
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-
-        // fromline까지 불필요한 줄 읽기
         for (int i = 0; i < fromLine - 1; i++) {
             reader.readLine();
         }
 
-        // 데이터 읽기
         StringBuilder stringBuilder = new StringBuilder();
         String line;
         while ((line = reader.readLine()) != null) {
@@ -47,14 +59,6 @@ public class JsonReader {
         if (json.equals("")) {
             throw new JsonEmptyException("Json URL is empty or it damaged!");
         }
-        return new JSONObject(json);
-    }
-
-    public static JSONObject readFromFile(String path) throws IOException {
-        String json = "";
-        json = new String(
-                Files.readAllBytes(Paths.get(path))
-        );
         return new JSONObject(json);
     }
 }

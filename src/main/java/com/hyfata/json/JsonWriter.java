764b79e6b06fc54092cd1075997d93c042f3f1c2
Najoan <68422289+najoan125@users.noreply.github.com>
2023-08-21 19:19:54 +0900

    fix code

--- src/main/java/com/hyfata/json/JsonWriter.java
+++ src/main/java/com/hyfata/json/JsonWriter.java
@@ -1,14 +1,17 @@
 package com.hyfata.json;
 
-import java.io.FileOutputStream;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import org.json.JSONObject;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class JsonWriter {
     public static void writeToFile(JSONObject obj, String path) throws IOException {
-        try (PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(path), "UTF-8"))) {
+        try (PrintWriter out = new PrintWriter(new OutputStreamWriter(Files.newOutputStream(Paths.get(path)), StandardCharsets.UTF_8))) {
             out.write(obj.toString());
         }
     }
